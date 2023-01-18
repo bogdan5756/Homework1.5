3f2db4bdb01d18bd78aeb916c8753928bf2ecd03
dimab <dima.bogdashkin@gmail.com>
2023-01-18 16:09:56 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+    public static void task1() {
+        System.out.println("Задание 1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+    public static void task2() {
+        System.out.println("Задание 2");
+        int clientOS = 0;
+        int clientDeviceYear = 2018;
+        if (clientOS == 0 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+    public static void task3() {
+        System.out.println("Задание 3");
+    }
+    public static void task4() {
+        System.out.println("Задание 4");
+    }
+    public static void task5() {
+        System.out.println("Задание 5");
     }
 }
